cefadf44e45970ae19a7dbca19aca52761e0e1e1
Kate Jefferson <barbedbella@gmail.com>
2015-03-25 13:57:44 -0500

    (fix) installed firebase via node (feat) setup firebase

---
+++ server/firebaseTest.js
@@ -0,0 +1,19 @@
+/**
+ * Created by kate on 3/25/15.
+ */
+var Firebase = require('firebase');
+
+var ref = new Firebase('https://gitinsights.firebaseio.com');
+
+var list = ref.child("candidates");
+
+list.set({
+  candidate1: {
+    github: 'barbbella',
+    name: 'Kate Jefferson'
+  },
+  candidate2: {
+    github: 'johnnygames',
+    name: 'John Games'
+  }
+});
