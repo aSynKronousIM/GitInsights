aa1c293dc5715b385c9842c6d30048ad1b2ee363
Kate Jefferson <barbedbella@gmail.com>
2015-03-26 13:27:56 -0500

    (fix) added scripts to index for angular fire (feat) added angularfireauth controller (feat) added angularfire controller for favorites list

--- server/firebaseTest.js
+++ server/firebaseTest.js
@@ -1,19 +1,45 @@
 /**
  * Created by kate on 3/25/15.
  */
-var Firebase = require('firebase');
+//var Firebase = require('firebase');
+//
+//var ref = new Firebase('https://gitinsights.firebaseio.com');
+//
+//var list = ref.child("list");
+//
+//list.set({
+//  candidate1: {
+//    github: 'barbbella',
+//    name: 'Kate Jefferson'
+//  },
+//  candidate2: {
+//    github: 'johnnygames',
+//    name: 'John Games'
+//  }
+//});
 
-var ref = new Firebase('https://gitinsights.firebaseio.com');
+/**
+ * Firebase module
+ * @type {module|*}
+ */
+var db = angular.module('Firebase', ['firebase']);
+// makes $firebaseObject, $firebaseArray, and $firebaseAuth available
 
-var list = ref.child("list");
+/**
+ * Firebase controller for favorite list
+ */
+db.controller('ListController', function($scope, $firebaseArray) {
+  var ref = new Firebase('https://gitinsights.firebaseio.com/favorites');
 
-list.set({
-  candidate1: {
-    github: 'barbbella',
-    name: 'Kate Jefferson'
-  },
-  candidate2: {
-    github: 'johnnygames',
-    name: 'John Games'
-  }
+  // creates synchronized array
+  // do not use array methods
+  // use $add() $save() and $remove()
+  $scope.favorites = $firebaseArray(ref);
+
+  $scope.addFavorite = function () {
+    $scope.favorites.$add({
+      username: $scope.username
+    });
+  };
 });
+
